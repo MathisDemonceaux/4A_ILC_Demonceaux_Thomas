4d187e62e927a7e91467d614a32ef8dfc46cca3e
Mathis Demonceaux <md622777@ad.u-bourgogne.fr>
2023-01-12 11:30:09 +0100

    Add Transactions Endpoints

--- index.js
+++ index.js
@@ -15,15 +15,42 @@ app.get('/', (req,res)=>{
 app.get('/personnes', (req, res) => {
   return res.send(personnes);
 });
+
+app.get('/transactions', (req, res) => {
+    return res.send(transactions);
+  });
+
 app.post('/personnes', (req, res) => {
     const personne = {
         id: personnes.length + 1,
         nom: req.body.nom,
         prenom: req.body.prenom,
+        solde: parseInt(req.body.solde || 0) 
     }
     personnes.push(personne);
     return res.send(personne);
 });
+
+app.post('/transactions', (req, res) => {
+    const P1 = personnes.find(p => p.id === parseInt(req.body.P1));
+    const P2 = personnes.find(p => p.id === parseInt(req.body.P2));
+    if(!P1 || !P2 || P1.solde-parseInt(req.body.somme) >=0){
+        const transaction = {
+            P1: req.body.P1,
+            P2: req.body.P2,
+            t: Date.now(),
+            s: parseInt(req.body.somme)
+        }
+        P1.solde = P1.solde - parseInt(req.body.somme);
+        P2.solde = P2.solde + parseInt(req.body.somme);
+        transactions.push(transaction);
+        return res.send(transaction);
+    }
+    else{
+        return res.status(404).send('La personne P1 n\'a pas assez d\'argent');
+    }
+});
+
 app.delete('/personnes/:id', (req, res) => {
     const personne = personnes.find(p => p.id === parseInt(req.params.id));
     if (!personne) {
@@ -35,5 +62,6 @@ app.delete('/personnes/:id', (req, res) => {
         return res.send(personne);
     }
 });
+
 
 app.listen(3000);
